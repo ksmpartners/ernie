db96744412b8ee859b211ea0796e9f1a4c6bd926
Andy Albrecht <aalbrecht@ksmpartners.com>
2013-02-18 12:43:41 -0500

    Add test implementation using Jackson JSON.

--- ernie-model/src/test/java/com/ksmpartners/ernie/model/TestTest.java
+++ ernie-model/src/test/java/com/ksmpartners/ernie/model/TestTest.java
@@ -1,13 +1,55 @@
 package com.ksmpartners.ernie.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestTest {
 
     @Test
     public void testMethod()
+        throws Exception
     {
-        return;
+        ObjectMapper om = new ObjectMapper();
+
+        TestClass obj = new TestClass();
+
+        obj.setName("test_name");
+        obj.setId(1);
+
+        String json = om.writeValueAsString(obj);
+
+        Assert.assertEquals(json, "{\"name\":\"test_name\",\"id\":1}");
+    }
+
+    private class TestClass {
+
+        private String name;
+        private int id;
+
+        @JsonProperty("name")
+        public String getName()
+        {
+            return name;
+        }
+
+        @JsonProperty
+        public int getId()
+        {
+            return id;
+        }
+
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+
+        public void setId(int id)
+        {
+            this.id = id;
+        }
+
     }
 
 }
