b461d9a637bb56bd0afaff862865f23bd19bd6ea
Andy Albrecht <aalbrecht@ksmpartners.com>
2013-02-20 14:32:06 -0500

    Add test for serializing a List<TestClass>.

--- ernie-model/src/test/java/com/ksmpartners/ernie/model/TestTest.java
+++ ernie-model/src/test/java/com/ksmpartners/ernie/model/TestTest.java
@@ -1,15 +1,17 @@
 package com.ksmpartners.ernie.model;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.ksmpartners.ernie.util.TestUtil;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class TestTest {
 
     @Test
-    public void testMethod()
+    public void testTestClass()
         throws Exception
     {
         TestClass obj = new TestClass("test_name", 1);
@@ -20,6 +22,24 @@ public class TestTest {
         Assert.assertEquals(newObj.getName(), "test_name");
 
         Assert.assertEquals(json, "{\"name\":\"test_name\",\"id\":1}");
+    }
+
+    @Test
+    public void testLists()
+            throws Exception
+    {
+        TestClass obj1 = new TestClass("test_name1", 1);
+        TestClass obj2 = new TestClass("test_name2", 2);
+
+        List<TestClass> objList = new ArrayList<TestClass>();
+
+        objList.add(obj1);
+        objList.add(obj2);
+
+        String json = TestUtil.serialize(objList);
+        List<TestClass> newList = TestUtil.MAPPER.readValue(json, new TypeReference<List<TestClass>>(){});
+
+        Assert.assertEquals(json, "[{\"name\":\"test_name1\",\"id\":1},{\"name\":\"test_name2\",\"id\":2}]");
     }
 
     @Test
