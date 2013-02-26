1f19bbcde9b1e4a4b8d6f7ac12ed548ba2e189ff
Andy Albrecht <aalbrecht@ksmpartners.com>
2013-02-26 18:05:37 -0500

    Add logback libs.

--- ernie-server/src/main/scala/com/ksmpartners/ernie/server/DispatchRestAPI.scala
+++ ernie-server/src/main/scala/com/ksmpartners/ernie/server/DispatchRestAPI.scala
@@ -5,8 +5,11 @@ import net.liftweb.common.{Box,Empty,Failure,Full,Logger}
 
 import scala.xml._
 import net.liftweb.http._
+import org.slf4j.LoggerFactory
 
 object DispatchRestAPI extends XMLApiHelper {
+
+  private val LOG = LoggerFactory.getLogger(this.getClass)
 
   // Required override, though not used
   def createTag(contents : NodeSeq) = <api>{contents}</api>
@@ -18,7 +21,9 @@ object DispatchRestAPI extends XMLApiHelper {
     case Req(List("jobs"), _, PutRequest) => () => Full(OkResponse())
     case Req(List("jobs", jobId, "status"), _, GetRequest) => () => Full(OkResponse())
     case Req(List("jobs", jobId, "results", "pdf"), _, GetRequest) => () => Full(OkResponse())
-    case _ => () => Full(NotFoundResponse())
+    case _ =>
+      LOG.error("Got unknown request")
+      () => Full(NotFoundResponse())
   }
 
 }
