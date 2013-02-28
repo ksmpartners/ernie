3ab8d1bf54e798606dd8e0ed5f1545819d039619
Andy Albrecht <aalbrecht@ksmpartners.com>
2013-02-28 15:41:08 -0500

    Add getJsonResponse method to JsonTranslator.

--- ernie-server/src/main/scala/com/ksmpartners/ernie/server/JobDependencies.scala
+++ ernie-server/src/main/scala/com/ksmpartners/ernie/server/JobDependencies.scala
@@ -1,7 +1,7 @@
 package com.ksmpartners.ernie.server
 
 import net.liftweb.common.{Box, Full, Empty}
-import net.liftweb.http.{PlainTextResponse, OkResponse}
+import net.liftweb.http.{LiftResponse, PlainTextResponse, OkResponse}
 import com.ksmpartners.ernie.engine.{StatusRequest => SReq, Coordinator, ReportGenerator, ReportRequest => RReq}
 import com.ksmpartners.ernie.model.{Notification, ReportRequest}
 import com.fasterxml.jackson.databind.ObjectMapper
@@ -11,21 +11,21 @@ trait JobDependencies {
   val reportGenerator = new ReportGenerator(".", ".") // TODO: Make directories a configuration
   val coordinator = new Coordinator(reportGenerator).start()
 
-  class JobsResource extends JsonTranslator{
+  class JobsResource extends JsonTranslator {
     def get = {
       Full(OkResponse())
     }
     def put(body: Box[Array[Byte]]) = {
       val obj = deserialize(body.open_!, classOf[ReportRequest])
       val response = (coordinator !! RReq(obj.getReportDefId))
-      Full(PlainTextResponse(serialize(response.apply().asInstanceOf[Notification]), List(("Content-Type", "application/json")), 200))
+      getJsonResponse(response.apply().asInstanceOf[Notification])
     }
   }
 
-  class JobStatusResource extends JsonTranslator{
+  class JobStatusResource extends JsonTranslator {
     def get(jobId: String) = {
       val response = (coordinator !! SReq(jobId.toInt))
-      Full(PlainTextResponse(serialize(response.apply().asInstanceOf[Notification]), List(("Content-Type", "application/json")), 200))
+      getJsonResponse(response.apply().asInstanceOf[Notification])
     }
   }
 
@@ -56,5 +56,9 @@ trait JsonTranslator {
       case t: T => t
       case _ => throw new ClassCastException
     }
+  }
+
+  def getJsonResponse[T](response: T): Box[LiftResponse] = {
+    Full(PlainTextResponse(serialize(response), List(("Content-Type", "application/json")), 200))
   }
 }
