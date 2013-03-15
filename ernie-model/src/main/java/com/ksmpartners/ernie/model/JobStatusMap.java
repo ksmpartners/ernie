1b839cd17fe87a20e01c52a6612d3f0f8d05972a
Andy Albrecht <aalbrecht@ksmpartners.com>
2013-03-15 11:56:28 -0400

    Fix documentation on .java files.

--- ernie-model/src/main/java/com/ksmpartners/ernie/model/JobStatusMap.java
+++ ernie-model/src/main/java/com/ksmpartners/ernie/model/JobStatusMap.java
@@ -1,7 +1,17 @@
+/**
+ * This source code file is the intellectual property of KSM Technology Partners LLC.
+ * The contents of this file may not be reproduced, published, or distributed in any
+ * form, except as allowed in a license agreement between KSM Technology Partners LLC
+ * and a licensee. Copyright 2012 KSM Technology Partners LLC.  All rights reserved.
+ */
+
 package com.ksmpartners.ernie.model;
 
 import java.util.Map;
 
+/**
+ * A JSONable class used to send job status information via HTTP
+ */
 public class JobStatusMap {
 
     private Map<Long, JobStatus> jobStatusMap;
--- ernie-model/src/main/java/com/ksmpartners/ernie/model/ReportResponse.java
+++ ernie-model/src/main/java/com/ksmpartners/ernie/model/ReportResponse.java
@@ -1,5 +1,5 @@
 /**
- * This source code file is the intellectual property of KSM Technology Partners LLC.  
+ * This source code file is the intellectual property of KSM Technology Partners LLC.
  * The contents of this file may not be reproduced, published, or distributed in any
  * form, except as allowed in a license agreement between KSM Technology Partners LLC
  * and a licensee. Copyright 2012 KSM Technology Partners LLC.  All rights reserved. 
