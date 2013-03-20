9957bec3278b4ef0e921d7c9898eb37e1f3d6f7c
Andy Albrecht <aalbrecht@ksmpartners.com>
2013-03-20 10:05:14 -0400

    Add standard header to new source files.

--- ernie-server/src/main/scala/com/ksmpartners/ernie/server/DefinitionDependencies.scala
+++ ernie-server/src/main/scala/com/ksmpartners/ernie/server/DefinitionDependencies.scala
@@ -1,3 +1,10 @@
+/**
+ * This source code file is the intellectual property of KSM Technology Partners LLC.
+ * The contents of this file may not be reproduced, published, or distributed in any
+ * form, except as allowed in a license agreement between KSM Technology Partners LLC
+ * and a licensee. Copyright 2012 KSM Technology Partners LLC.  All rights reserved.
+ */
+
 package com.ksmpartners.ernie.server
 
 import com.ksmpartners.ernie.{ model, engine }
