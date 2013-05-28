2d5006b4cb0c4314a9ab36f4be7d885caf36222a
Adam Coimbra <acoimbra@ksmpartners.com>
2013-05-28 14:03:31 -0400

    Coordinator map now uses JobEntity model

---
+++ ernie-model/src/main/java/com/ksmpartners/ernie/model/JobEntity.java
@@ -0,0 +1,68 @@
+/**
+ * This source code file is the intellectual property of KSM Technology Partners LLC.  
+ * The contents of this file may not be reproduced, published, or distributed in any
+ * form, except as allowed in a license agreement between KSM Technology Partners LLC
+ * and a licensee. Copyright 2012 KSM Technology Partners LLC.  All rights reserved. 
+ */
+
+package com.ksmpartners.ernie.model;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.ksmpartners.ernie.util.ISODateDeserializer;
+import com.ksmpartners.ernie.util.ISODateSerializer;
+import org.joda.time.DateTime;
+
+import java.util.List;
+
+/**
+ * A JSONable class used to serialize definition meta-data to disk
+ */
+public class JobEntity extends ModelObject {
+
+    private Long jobId;
+    private String rptId;
+    private JobStatus jobStatus;
+    private ReportType rptType;
+
+    public JobEntity() {}
+
+    public JobEntity(Long jobId, String rptId, JobStatus jobStatus, ReportType rptType) {
+        this.jobId = jobId;
+        this.rptId = rptId;
+        this.jobStatus = jobStatus;
+        this.rptType = rptType;
+    }
+
+    public Long getJobId() {
+        return jobId;
+    }
+
+    public String getRptId() {
+        return rptId;
+    }
+
+    public JobStatus getJobStatus() {
+        return jobStatus;
+    }
+
+    public ReportType getRptType() {
+        return rptType;
+    }
+    public void setJobId(Long jobId) {
+        this.jobId = jobId;
+    }
+
+    public void setRptId(String rptId) {
+        this.rptId = rptId;
+    }
+
+    public void setJobStatus(JobStatus jobStatus) {
+        this.jobStatus = jobStatus;
+    }
+
+    public void setRptType(ReportType rptType) {
+        this.rptType = rptType;
+    }
+
+}
