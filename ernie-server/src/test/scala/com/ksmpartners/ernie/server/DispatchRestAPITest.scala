6f15056eb2ee094ff8abaf1b0468f30d9a395d6c
Adam Coimbra <acoimbra@ksmpartners.com>
2013-05-30 16:08:55 -0400

    Test push

--- ernie-server/src/test/scala/com/ksmpartners/ernie/server/DispatchRestAPITest.scala
+++ ernie-server/src/test/scala/com/ksmpartners/ernie/server/DispatchRestAPITest.scala
@@ -3,7 +3,7 @@
  * The contents of this file may not be reproduced, published, or distributed in any
  * form, except as allowed in a license agreement between KSM Technology Partners LLC
  * and a licensee. Copyright 2012 KSM Technology Partners LLC.  All rights reserved.
- */
+  */
 
 package com.ksmpartners.ernie.server
 
