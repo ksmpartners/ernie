1929bee11bb6b13e1232d7833633dd9d75e9ec0e
Adam Coimbra <adamcoimbra@gmail.com>
2013-07-29 08:47:31 -0400

    Fixed SBT build to use local milyn pom

--- run-web-server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mvn jetty:run -f ernie-server/pom.xml \
-  -Dernie.props="./ernie-server/src/main/resources/props/default.props" \
-  -Dkeystore.location="./ernie-server/src/test/resources/keystore.jks" \
-  -Dauthentication.mode="SAML"
